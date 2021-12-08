cde5cbedb648042dc11c833740e74aeb949c72fb
Pratyush-98 <pratyushsharma899@msitprogram.net>
2021-12-08 10:06:14 +0530

    completed arr-pop

--- Steque.java
+++ Steque.java
@@ -99,8 +99,9 @@ public class Steque<Item> implements Iterable<Item>
      * checks to see if steque is empty.
      * @return true if steque is empty, false otherwise.
      */
-    public boolean isEmpty() {
-
+    public boolean isEmpty() 
+    {
+        return first==null || last ==null;
     }
     
     /**
