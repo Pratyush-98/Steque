f3c21c3fbc36d33accfb7f40a703519f6becaae9
Pratyush-98 <pratyushsharma899@msitprogram.net>
2021-12-08 10:09:51 +0530

    completed arr-iterator

--- Steque.java
+++ Steque.java
@@ -123,7 +123,41 @@ public class Steque<Item> implements Iterable<Item>
      * stored in steque.
      * 
      */
-    public Iterator<Item> iterator() {
+    public Iterator<Item> iterator() 
+    {
+        return new StequeIterator();
+    }
+    public class StequeIterator implements Iterator<Item> 
+    {
+        Node current = first;
+        public boolean hasNext() 
+        {
+            return current!=null;
+        }
+        public Item next()
+        {
+            if(!hasNext()) throw new NoSuchElementException();
+            else
+            {
+                Item item = current.item;
+                current = current.next;
+                return item;
+            }
+    
+        }
+        public void remove()
+        {
+            throw new UnsupportedOperationException();
+        }
 
+        public  static void main(String args[])
+        {
+           Steque<Integer> st = new Steque<Integer>();
+           st.enqueue(2);
+           st.push(3);
+           st.pop();
+           st.size();
+           st.isEmpty();
+        }
     }
 }
